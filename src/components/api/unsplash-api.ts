c845580073d9d6a00dd983ebc8a317b363988599
AnnaLvchnk <nlvcnk@gmail.com>
2024-05-29 16:55:51 +0200

    task completed

--- src/components/api/unsplash-api.ts
+++ src/components/api/unsplash-api.ts
@@ -4,6 +4,6 @@ const ACCESSKEY = 'WHLnFwCnRU_hgkU0QvtphikdKCcX_8IYP7sGB2zr7Ow';
 
 axios.defaults.baseURL = "https://api.unsplash.com/search";
 
-export const getFetchUrl = ( page, per_page, query) => {
+export const getFetchUrl = ( page: number, per_page: number, query: string): string => {
   return `/photos?client_id=${ACCESSKEY}&page=${page}&per_page=${per_page}&query=${query}`;
 }
